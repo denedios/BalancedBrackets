5d37debf6176ae4df9a55955671df09c627648c6
Chris Denedios <cdenedios@therapeuticsmd.com>
2017-08-21 16:46:54 -0400

    no message

--- src/BalancedBrackets/Program.cs
+++ src/BalancedBrackets/Program.cs
@@ -1,31 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BalancedBrackets
 {
     class Program
     {
+        private static readonly char[] LeftBrackets = { '{', '(', '[' };
+        private static readonly char[] RightBrackets = { '}', ')', ']' };
+        private static readonly Dictionary<char, char> BracketMap = new Dictionary<char, char>()
+        {
+            {'}', '{'},
+            {']', '['},
+            {')', '('}
+        };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-
-
-
-
-
-
-
-
-
+            Console.WriteLine("Enter your brackets - ie {{[]]]((( :");
+            var input = Console.ReadLine();
+            Console.WriteLine($"Is this bracket ballanced? {input}");
+            Console.WriteLine($"{IsBalanced(input)}!");
             Console.ReadLine();
         }
 
-
-
-
-        private static bool IsBalanced(string brackets)
+        private static bool IsBalanced(string source)
         {
-            
+            Stack<char> brackets = new Stack<char>();
+
+            foreach (char bracket in source)
+            {
+                if (BracketMap.ContainsValue(bracket))
+                    brackets.Push(bracket);
+
+                if (BracketMap.ContainsKey(bracket) && (brackets.Count == 0 || brackets.Pop() != BracketMap[bracket]))
+                    return false;
+            }
+
+            return brackets.Count == 0;
         }
     }
 }
